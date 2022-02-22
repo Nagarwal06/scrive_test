d6d7f59d6482d5caa3f5863160fd137c18d66f9f
Nitisha Agarwal <45269697+Nagarwal06@users.noreply.github.com>
2022-02-23 00:42:31 +0530

    Update gui_constants.py

--- scrive/testcases/gui_constants.py
+++ scrive/testcases/gui_constants.py
@@ -5,7 +5,7 @@ BROWSER_NAME = "chrome"
 DOCUMENT_NAME = "test_document"
 PLATFORM = os.environ.get('PLATFORM', 'WIN')
 SCREENSHOT_PATH = os.environ.get('SCREENSHOT_PATH', 'C:/Users/document_screenshot.png' if PLATFORM == 'WIN' else '/tmp/document_screenshot.png')
-DRIVER_LOCATION = os.environ.get('DRIVER_LOCATION', r'C:\Program Files\Python37\chromedriver.exe' if PLATFORM == 'WIN' else '/tmp/scrive_test/chromedriver')
+DRIVER_LOCATION = os.environ.get('DRIVER_LOCATION', r'C:\Program Files\Python37\chromedriver.exe' if PLATFORM == 'WIN' else '/tmp/scrive/chromedriver')
 TIMEOUT = 5
 
 # XPATH
@@ -14,5 +14,3 @@ NEXT_BTN_XPATH = ".//span[contains(text(),'Next')]"
 SIGN_MODAL_XPATH = ".//div[@class='main']//div[contains(@class,'above-overlay')]"
 SIGN_BTN_XPATH = ".//div[@class='label']//span[contains(text(),'Sign')]"
 SIGNED_DOCUMENT_XPATH = ".//h1[@class='follow']/span"
-
-
